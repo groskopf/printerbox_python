c5445aa309a32a2ba8c077dacf25b0219f0a29e4
Paul Groskopf <paul@groskopf-embedded.com>
2021-06-21 21:40:03 +0200

    clean up

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,3 @@
-docker run -it --rm --name python --network printerbox_network -v $(pwd)/../printerbox_cupsd/labels:/labels -v $(pwd)/../printerbox_sortkaffe/src:/src --entrypoint python printbox_python /src/printbox.py
+#!/bin/bash
+docker run -it --rm --name python --network printerbox_network -v $(pwd)/../printerbox_cupsd/labels:/labels -v $(pwd)/../printerbox_sortkaffe/src:/src --entrypoint python printerbox_sortkaffe_sortkaffe $1
 
